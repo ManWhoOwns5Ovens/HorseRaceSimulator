0d54115cbd6bdd5c8c3ac65ca77413bcd3e29410
ManWhoOwns5Ovens <valsandavid2006@gmail.com>
2025-04-11 16:44:01 +0100

    Keep main and gui-development branches consistent with each other.

--- Part2/LanePanel.java
+++ Part2/LanePanel.java
@@ -4,25 +4,48 @@ import javax.swing.*;
 public class LanePanel extends JPanel {
     private Horse horse;
     private char symbol;
+    private int raceLength;
 
-    public LanePanel(Horse horse){
+    public LanePanel(Horse horse, int raceLength) {
         this.horse = horse;
-        this.symbol = horse.getSymbol();
-        this.setBackground(Color.GREEN);
+        setSymbol();
+        this.raceLength=raceLength;
     }
 
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.setFont(new Font("SansSerif", Font.PLAIN, 15));
-        g.drawString(this.symbol+"", horse.getDistanceTravelled()*25, 12); //examine
+
+        g.drawRect(0,0, (raceLength*25+25)-1, getHeight()-1); //borders of the lane
+        g.drawRect(raceLength*25,0, 25, getHeight()-1); //draw borders of last spot/ finish line
+
+        if(this.horse!= null){
+            g.setFont(new Font("SansSerif", Font.PLAIN, 15));
+            g.drawString(this.symbol+"", horse.getDistanceTravelled()*25, 17); //examine
+
+            g.setFont(new Font("SansSerif", Font.PLAIN, 10));
+            g.drawString(horse.getName()+" (Current confidence "+horse.getConfidence()+")",25*raceLength+35, 15);
+        }
+        
     }
 
     public void updateLane(){
-        if(horse.hasFallen()){
-            this.symbol='❌';
-        }
+        setSymbol();
         repaint();
+    }
+
+    private void setSymbol(){
+        if(this.horse!=null){
+            if(this.horse.hasFallen()){
+                this.symbol='❌';
+            }
+            else{
+                this.symbol=horse.getSymbol();
+            }
+        }
+        else{
+            this.symbol=' ';
+        }
     }
 
 }
